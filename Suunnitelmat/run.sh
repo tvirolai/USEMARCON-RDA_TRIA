27fd12afea89110a2ff8ff9698679bee12b5d345
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2015-08-12 09:54:22 +0300

    Sidosasujen lyhenteiden avaus

--- Suunnitelmat/run.sh
+++
@@ -1,1 +0,0 @@
-/home/tuomo/usemarcon317/program/usemarcon /home/tuomo/Dropbox/Koodi/USEMARCON-konversiot/USEMARCON-RDA/ma21RDA.ini /home/tuomo/Dropbox/Koodi/USEMARCON-konversiot/USEMARCON-RDA/Suunnitelmat/BWpoimittuja.bib ./output.mrc
